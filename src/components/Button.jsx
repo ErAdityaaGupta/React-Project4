c81f4385162145932d93615a4cc3ffb8322970a3
Aditya Gupta <ingenious.adig@gmail.com>
2024-09-26 23:18:59 +0530

    Created the Hero Section

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button({ label, iconUrl }) {
+  return (
+    <button className="bg-coral-red  py-4 rounded-3xl text-white flex justify-center items-center gap-2 px-7 font-montserrat leading-none ">
+      {label}
+      <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full h-5 w-5' />
+    </button>
+  );
+}
