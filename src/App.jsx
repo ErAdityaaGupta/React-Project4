99d7bcac29fe71dd792b6da8fb885fe67d2a5047
Aditya Gupta <ingenious.adig@gmail.com>
2024-09-26 18:08:33 +0530

    Created all the services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import CustomerReviews from "./Sections/CustomerReviews";
+import Footer from "./Sections/Footer";
+import Hero from "./Sections/Hero";
+import PopularProducts from "./Sections/PopularProducts";
+import Services from "./Sections/Services";
+import SpecialOffers from "./Sections/SpecialOffers";
+import Subscribe from "./Sections/Subscribe";
+import SuperQuality from "./Sections/SuperQuality";
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
-    </>
+    <main className="relative">
+      {/* <Nav></Nav> */}
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero></Hero>
+      </section>
+      <section className="padding">
+        <PopularProducts></PopularProducts>
+      </section>
+      <section className="padding">
+        <SuperQuality></SuperQuality>
+      </section>
+      <section className="padding-x py-10">
+        <Services></Services>
+      </section>
+      <section className="padding">
+        <SpecialOffers></SpecialOffers>
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews></CustomerReviews>
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe></Subscribe>
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer></Footer>
+      </section>
+    </main>
   );
 }
