ece90483d9b6a85877f55f3bc981ec29402d3836
Aditya Gupta <ingenious.adig@gmail.com>
2024-09-29 23:55:08 +0530

    Created the Super Quality Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ label, iconUrl }) {
   return (
     <button className="bg-coral-red  py-4 rounded-3xl text-white flex justify-center items-center gap-2 px-7 font-montserrat leading-none ">
       {label}
-      <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full h-5 w-5' />
+      { iconUrl&& <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full h-5 w-5' />}
     </button>
   );
 }
