bfe293fb1f07b74e372beba641e33be160ede17c
Aditya Gupta <ingenious.adig@gmail.com>
2024-09-30 21:23:40 +0530

    Created the Special Offer Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function Button({ label, iconUrl }) {
+export default function Button({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) {
   return (
-    <button className="bg-coral-red  py-4 rounded-3xl text-white flex justify-center items-center gap-2 px-7 font-montserrat leading-none ">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : "bg-coral-red text-white border-coral-red"
+      }`}
+    >
       {label}
-      { iconUrl&& <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full h-5 w-5' />}
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right icon"
+          className="ml-2 rounded-full h-5 w-5"
+        />
+      )}
     </button>
   );
 }
